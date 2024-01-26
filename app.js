b2734898fec85681345b58d8c0d1aaba24bb141e
unknown <aminezouguig@proton.me>
2024-01-26 03:09:17 +0100

    Level 1 Register Users with Username and Password

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');	
+
 
 const app = express();
 
@@ -12,7 +14,14 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+mongoose.connect("mongodb://127.0.0.1:27017/UsersDB", {useNewUrlParser: true});
 
+const userSchema = mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get('/', function (req, res)  {
     res.render("home")
@@ -24,6 +33,39 @@ app.get('/login', function (req, res)  {
 
 app.get('/register', function (req, res)  {
     res.render("register");
+});
+
+app.post('/register', function (req, res)  {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function (err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    })
+    
+});
+
+app.post('/login', function (req, res)  {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function (err, foundUser) {
+        if(err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('Secrets')
+                }
+            }
+        }
+    })
 });
 
 
